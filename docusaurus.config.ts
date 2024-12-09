6bd113a80b719cc5f2bb60297d1c2ac41eb2ebe7
David Nguyen <h250694@gmail.com>
2024-12-09 13:24:16 +0700

    chore: add Roboto font to configuration and custom CSS

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -63,6 +63,29 @@ const config: Config = {
       } satisfies Preset.Options,
     ],
   ],
+  headTags: [
+    {
+      tagName: "link",
+      attributes: {
+        rel: "preconnect",
+        href: "https://fonts.googleapis.com",
+      },
+    },
+    {
+      tagName: "link",
+      attributes: {
+        rel: "preconnect",
+        href: "https://fonts.gstatic.com",
+      },
+    },
+    {
+      tagName: "link",
+      attributes: {
+        rel: "preconnect",
+        href: "https: //fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap",
+      },
+    },
+  ],
   plugins: [require.resolve("docusaurus-plugin-image-zoom")],
   themeConfig: {
     // Replace with your project's social card
