daec3d0953482a8f0b62af1c2a8c9f8d5cc88cb2
David Nguyen <h250694@gmail.com>
2024-12-10 09:03:42 +0700

    chore: refactor font integration and add Apache License for font usage

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -78,13 +78,6 @@ const config: Config = {
         href: "https://fonts.gstatic.com",
       },
     },
-    {
-      tagName: "link",
-      attributes: {
-        rel: "preconnect",
-        href: "https: //fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap",
-      },
-    },
   ],
   plugins: [require.resolve("docusaurus-plugin-image-zoom")],
   themeConfig: {
